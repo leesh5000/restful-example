f9aeb26ebcf9d9a5867058786e55345f8565a7a8
leesh5000 <leesh50000@gmail.com>
2022-01-28 23:35:09 +0900

    hello rest!

---
+++ src/test/java/leesh/study/hellorest/HelloRestApplicationTests.java
@@ -0,0 +1,13 @@
+package leesh.study.hellorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
